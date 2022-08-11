2e6aefb08a63b5f7455c10619309ca93eca07d1d
AnilPatamber <anilpatamber@gmail.com>
2022-08-11 16:36:12 +0530

    Seaparte method addProduts created to insert data into the DB and is working fine

--- src/main/java/org/shoppingapplication/Main.java
+++ src/main/java/org/shoppingapplication/Main.java
@@ -10,23 +10,28 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("List of all the products");
+        /*System.out.println("List of all the products");
         List<Product> productList = new ArrayList<>();
-        productList.add(new Product(1,"Lenovo thinkPad", 55_000));
-        productList.add(new Product(2,"Dell Inspiron", 70_000));
-        productList.add(new Product(3,"HP Pavilion", 50_000));
-        productList.add(new Product(4,"Apple McBook Pro", 80_000));
+        productList.add(new Product(5, "Asus TUF A15", 65_000));
+        productList.add(new Product(6, "Dell Inspiron", 70_000));
+        productList.add(new Product(3, "HP Pavilion", 50_000));
+        productList.add(new Product(4, "Apple McBook Pro", 80_000));
         System.out.println(productList);
+        addProducts(productList);*/
+
+    }
+    public static void addProducts(List<Product> productList){
+
 
         //get Session factory object
         SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
         Session session = sessionFactory.openSession();
         session.beginTransaction();
-        for(Product prod : productList){
+        for (Product prod : productList) {
             session.save(prod);
         }
         session.getTransaction().commit();
+        sessionFactory.close();
         System.out.println("Products added to the database successfully!!!");
-
     }
 }
--- src/main/java/org/shoppingapplication/util/HibernateUtil.java
+++
@@ -1,5 +0,0 @@
-package org.shoppingapplication.util;
-
-public class HibernateUtil {
-
-}
